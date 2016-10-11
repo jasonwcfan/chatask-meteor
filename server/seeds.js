8e52a0b01dbe3a25d34e76c27e1112e500a02e0c
jasonwcfan <jason.wc.fan@gmail.com>
2016-10-10 21:40:47 -0400

    added chats methods

--- server/seeds.js
+++ server/seeds.js
@@ -1,10 +1,9 @@
 import Chats from '/lib/collections/chats';
 import Messages from '/lib/collections/messages';
 
-
 const seed = () => {
     if (Chats.find().count() === 0) {
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < 5; i++) {
             var chatId = Chats.insert({
                 name: `Chat #${i}`,
                 particpants: [{
